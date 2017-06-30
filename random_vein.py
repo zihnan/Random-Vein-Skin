2b8936b06b8daf8f00e8c94f740cb9608acf8f6f
zihnan <zwillis007@gmail.com>
2017-06-30 11:44:06 +0800

    fixed conv_2d

--- random_vein.py
+++ random_vein.py
@@ -1,6 +1,23 @@
 import math
 import numpy as np
 
+'''
+
+_, skin = random_vein_lines()
+tf = expand(skin, top_margin=1, left_margin=1)
+tr = expand(skin, top_margin=1, right_margin=1)
+bf = expand(skin, bottom_margin=1, left_margin=1)
+br = expand(skin, right_margin=1, bottom_margin=1)
+ex = expand(skin, margin=1)
+
+tfc = conv_2d(tf, skin.shape, top_margin=1, left_margin=1, mask=np.array([[1,1],[1,1]]))
+trc = conv_2d(tr, skin.shape, top_margin=1, right_margin=1, mask=np.array([[1,1],[1,1]]))
+bfc = conv_2d(bf, skin.shape, bottom_margin=1, left_margin=1, mask=np.array([[1,1],[1,1]]))
+brc = conv_2d(br, skin.shape, right_margin=1, bottom_margin=1, mask=np.array([[1,1],[1,1]]))
+exc = conv_2d(ex, skin.shape)
+
+'''
+
 def relu(matrix):
      return np.maximum(matrix, 0)
 
@@ -9,7 +26,6 @@ def logical_bool(matrix):
 
 def expand(matrix, margin=0, top_margin=0, bottom_margin=0, left_margin=0, right_margin=0):
     expand_matrix = np.zeros((matrix.shape[0] + margin*2 + (top_margin + bottom_margin), matrix.shape[1] + margin*2 + (left_margin + right_margin)))
-    print(expand_matrix.shape)
     expand_matrix[margin + top_margin:matrix.shape[0] + margin + top_margin, margin + left_margin:matrix.shape[1] + margin + left_margin] = matrix
     return expand_matrix
 
@@ -19,8 +35,7 @@ def conv_2d(expanded_matrix, shape, top_margin=1, right_margin=1, bottom_margin=
         for j in range(shape[1]):
             position_y = top_margin + i
             position_x = left_margin + j
-            temp = expanded_matrix[position_y - math.ceil(mask.shape[0]/2)+1:position_y + math.ceil(mask.shape[0]/2), position_x - math.ceil(mask.shape[1]/2)+1:position_x + math.ceil(mask.shape[1]/2)].reshape(mask.shape)
-            print(position_y, position_x)
+            temp = expanded_matrix[position_y - int(math.floor(mask.shape[0]/2.0)):position_y + int(math.ceil(mask.shape[0]/2.0)), position_x - int(math.floor(mask.shape[1]/2.0)):position_x + int(math.ceil(mask.shape[1]/2.0))].reshape(mask.shape)
             conv_matrix[i, j] = np.sum(temp * mask)
     return conv_matrix
 
@@ -44,17 +59,7 @@ def random_vein_lines():
              position_x = offset + i
              a = expand_relu_skin[position_x - 1:position_x +2,position_y - 1: position_y + 2]
              a = np.reshape(a, (-1,))
-             print(a)
-             print(position_x - 1,position_x + 2)
-             print(position_y - 1, position_y + 2)
-             print(np.dot(a, cross1.T))
-             print(np.dot(a, cross2.T))
              temp1 = relu(np.dot(a, cross1.T))
              temp2 = relu(np.dot(a, cross2.T))
-             print(temp1 , temp2)
-             print(np.logical_xor(temp1 > 0,temp2 > 0))
              result[i, j] = np.logical_xor(temp1 > 0,temp2 > 0)
-             print('>>>>>>>>>>>>>>>>>>>>>>>>>>')
-    print(relu(skin))
-    print(logical_bool(skin))
     return result, skin
